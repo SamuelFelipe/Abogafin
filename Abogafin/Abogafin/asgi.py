1c3047ab89c807ede14855235d12ed561e7152c8
SamuelFelipe <2354@holbertonschool.com>
2021-09-06 13:59:13 -0500

    first commit

---
+++ Abogafin/Abogafin/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Abogafin project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Abogafin.settings')
+
+application = get_asgi_application()
