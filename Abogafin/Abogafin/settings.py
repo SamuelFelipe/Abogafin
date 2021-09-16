b3180ad7f5f544610509c59c32dd9210e168e52a
Edheramirez <edher.ramirez@outlook.es>
2021-09-16 15:22:00 -0500

    Remove private key

--- Abogafin/Abogafin/settings.py
+++ Abogafin/Abogafin/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m!2p5_1ofpvrft69h@ph1ck%vbeir3j$vp3=^(6nsla-fk&u8%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
