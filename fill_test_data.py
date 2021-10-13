5603524f5170714376a1bd8df8111efeaef95aca
SamuelFelipe <2354@holbertonschool.com>
2021-10-12 23:53:37 -0500

    Update - actualize the rute to create test users

--- fill_test_data.py
+++ fill_test_data.py
@@ -5,11 +5,11 @@ from time import sleep
 import json
 
 
-
 with open('./mock_users.csv', mode='r') as f:
     for line in f:
-        data = line.split(',')
+        data = line.replace('\n', '').split(',')
         p_data = {
+                'email': data[0],
                 'first_name': data[1],
                 'last_name': data[2],
                 'cel': data[3],
@@ -17,10 +17,10 @@ with open('./mock_users.csv', mode='r') as f:
                 'doc_type': data[5],
                 'doc_number': data[6],
                 'city': data[7],
-                'email': data[0],
                 'password': data[8]
                 }
-        r = requests.post('http://127.0.0.1:8000/api/create_user',
+        r = requests.post('http://127.0.0.1:8000/signin',
                 data=json.dumps(p_data),
                 headers={'Content-Type': 'application/json'})
-        sleep(.2)
+        if r.status_code != 200:
+            print(r.content)
